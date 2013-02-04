4c753c5da885edc3abcfdf829b4e345f0b858639
Bryan <bryan@omicron.(none)>
2013-02-04 14:53:02 +0100

    Start reverse-binary.

---
+++ src/reverse_binary.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: bryan
+ * Date: 2/4/13
+ * Time: 2:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+public class reverse_binary {
+
+    public static void main(String[] args) {
+          System.out.println("hello world");
+    }
+}
