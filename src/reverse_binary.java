943bf23ed89346fc858fa5e22900521812c9602c
Bryan <bryan@omicron.(none)>
2013-02-10 23:44:30 +0100

    Read int array.

--- src/reverse_binary.java
+++ src/reverse_binary.java
@@ -1,14 +1,39 @@
 /**
- * Created with IntelliJ IDEA.
- * User: bryan
- * Date: 2/4/13
- * Time: 2:46 PM
- * To change this template use File | Settings | File Templates.
+ * @author bryan
+ * date: 2/10/13
  */
+
+import java.util.Scanner;
+import static java.lang.Integer.toBinaryString;
 
 public class reverse_binary {
 
     public static void main(String[] args) {
-          System.out.println("hello world");
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Please input your number:");
+
+        int intToConvert = scan.nextInt();
+        String intConverted = toBinaryString(intToConvert);
+        char[] arr = intConverted.toCharArray();
+        boolean[] binaryarray = new boolean[intConverted.length()];
+
+        for(int i; i<intConverted.length();i++){
+            if (arr[i] == '1'){
+                binaryarray[i] = true;
+            }
+            else if (arr[i] == '0'){
+                binaryarray[i] = false;
+            }
+        }
+
+
+        System.out.println("You entered:" + intConverted);
     }
+
+    public printFromArrat(String) {
+
+    }
+
 }
